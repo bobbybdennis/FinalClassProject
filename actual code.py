bc596d7e4a4585360dc8a185b76034874d30fa0b
bobbybdennis <32436921+bobbybdennis@users.noreply.github.com>
2018-12-01 13:31:35 -0500

    Update actual code.py
    This is a more polished version of the code.

--- actual code.py
+++ actual code.py
@@ -1,39 +1,62 @@
 import shell_command
+import subprocess
+import os
+import datetime
+
 from shell_command import shell_call
 from tkinter import *
 from tkinter import messagebox
 
-top = Tk()
-top.geometry("100x100")
+window = Tk()
+window.title("Command Buddy")
+window.geometry("100x100")
+window.configure(background='light blue')
+
+
 def Hostname():
-   shell_call("hostname")
-top.geometry("500x500")
+   shell_call("hostname & hostname >> Results.txt ")
+#def Hostname1():
+#    shell_call("hostname >> Results.txt")
+window.geometry("275x175")
 def GetMac():
-    shell_call("getmac")
+    shell_call("getmac & getmac >> Results.txt")
 def Ping():
-    shell_call("Ping www.google.com")
+    shell_call("Ping www.google.com & Ping www.google.com >> Results.txt")
 def GetIP():
-    shell_call("ipconfig")
+    shell_call("ipconfig & ipconfig >> results.txt")
 def FlushDNS():
-    shell_call("ipconfig /flushdns")
+    shell_call("ipconfig /flushdns & ipconfig /flushdns >> Results.txt")
 def ScanNow():
     shell_call("sfc /scannow")
-B1 = Button(top, text = "What is my Hostname?", command = Hostname)
-B1.place(x = 35,y = 50)
-B2 = Button(top, text = "Check if I can reach the Internet", command = Ping)
-B2.place(x = 50, y = 150)
-B3 = Button(top, text = "What is my MAC address", command = GetMac)
-B3.place(x = 50, y = 300)
-B4 = Button(top, text = "What is my IP?", command = GetIP)
-B4.place(x = 50, y = 450)
-B5 = Button(top, text = "Flush DNS Cache", command = FlushDNS)
-B5.place(x = 250, y = 450)
-B6 = Button(top, text = "Scan for Problems", command = ScanNow())
-B6.place(x = 450, y = 600)
-#B7 = Button(top, text = "ipconfig", command = GetIP)
-#B7.place(x = 50, y = 450)
-#B8 = Button(top, text = "ipconfig", command = GetIP)
+def TaskList():
+    shell_call("tasklist & tasklist >> Results.txt")
+def StarWars():
+    shell_call("telnet towel.blinkenlights.nl")
+def PowerSave():
+    shell_call("powercfg/energy & powercfg/energy >> Results.txt")
+def Date():
+    shell_call("date /t & time /t & date /t >> Results.txt & time /t >> Results.txt")
+#def ST1():         <-------- figure this out later. much later. maybe never
+    #cmd = 'python myprog.py 21 --mass 4'
+   # failure = subprocess.call(cmd, shell=True)
+
+    #os.system("start cmd")
+B1 = Button(window, text = "What is my Hostname?", command = Hostname)
+B1.place(x = 0,y = 0)
+B2 = Button(window, text = "Internet connectivity", command = Ping)
+B2.place(x = 0, y = 45)
+B3 = Button(window, text = "MAC address?", command = GetMac)
+B3.place(x = 0, y = 90)
+B4 = Button(window, text = "My IP address?", command = GetIP)
+B4.place(x = 150, y = 0)
+B5 = Button(window, text = "Flush DNS", command = FlushDNS)
+B5.place(x = 150, y = 45)
+B6 = Button(window, text = "Task list", command = TaskList)
+B6.place(x = 150, y = 90)
+B7 = Button(window, text = "Date and Time", command = Date)
+B7.place(x = 150, y = 130)
+#B8 = Button(window, text = "Starwars part 1", command = ST1)
 #B8.place(x = 50, y = 450)
-#B9 = Button(top, text = "ipconfig", command = GetIP)
+#B9 = Button(window, text = "ipconfig", command = GetIP)
 #B9.place(x = 50, y = 450)
-top.mainloop()
+window.mainloop()
